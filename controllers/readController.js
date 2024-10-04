1186fa49a90127d654f616f29d298459ccc80b82
jorted-runner <jorted.python@gmail.com>
2024-10-04 15:19:06 -0500

    Some error handling

--- controllers/readController.js
+++ controllers/readController.js
@@ -3,24 +3,33 @@ const ObjectId = require('mongodb').ObjectId;
 
 // Get all read books
 async function getAllReadBooks(req, res) {
-	const result = await mongodb.getDatabase().db().collection('books_read').find();
-	result.toArray().then((books) => {
-		res.setHeader('Content-Type', 'application/json');
-		res.status(200).json(books);
-	});
+    try {
+        const result = await mongodb.getDatabase().db().collection('books_read').find();
+        const books = await result.toArray();
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(books);
+    } catch (error) {
+        console.error('Error fetching books:', error);
+        res.status(500).json({ message: 'Failed to fetch books' });
+    }
 }
 
 // Add a book
 async function addBookRead(req, res) {
-    const newBook = {
-        Title: req.body.title,
-        Author: req.body.author,
-        Date_Finished: req.body.date_finished,
-        Rating: req.body.rating,
-        Review: req.body.review
+    try {
+        const newBook = {
+            Title: req.body.title,
+            Author: req.body.author,
+            Date_Finished: req.body.date_finished,
+            Rating: req.body.rating,
+            Review: req.body.review
+        };
+        const result = await mongodb.getDatabase().db().collection('books_read').insertOne(newBook);
+        res.status(200).json({ message: 'New read book added', bookId: result.insertedId });
+    } catch (error) {
+        console.error('Error adding new book:', error);
+        res.status(500).json({ message: 'Failed to add new book' });
     }
-    const result = await mongodb.getDatabase().db().collection('books_read').insertOne(newBook)
-    res.status(200).json('New read book added')
 }
 
 
--- routes/booksRead.js
+++ routes/booksRead.js
@@ -3,8 +3,10 @@ const router = express.Router()
 const { getAllReadBooks, addBookRead } = require('../controllers/readController')
 const validation = require('../middleware/validate')
 
+// Get all Read Books
 router.get('/', getAllReadBooks)
 
+// Add a new Read Book
 router.post('/', validation.saveBookRead, addBookRead);
 
 module.exports = router
