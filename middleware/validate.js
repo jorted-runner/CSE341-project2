1a110ab2bf3f8dc89c295b08b713d871f94d27de
joreted-runner <irunner1996@gmail.com>
2024-10-04 06:52:23 -0500

    Post request validation

---
+++ middleware/validate.js
@@ -0,0 +1,26 @@
+const validatorHelper = require('./helper');
+
+const saveBookRead = (req, res, next) => {
+	const validationRule = {
+		title: 'required|string',
+		author: 'required|string',
+		rating: 'required|numeric',
+		review: 'required|string',
+		date_finished: 'required|string',
+	};
+	validatorHelper(req.body, validationRule, {}, (err, status) => {
+		if (!status) {
+			res.status(412).send({
+				success: false,
+				message: 'Validation failed',
+				data: err,
+			});
+		} else {
+			next();
+		}
+	});
+};
+
+module.exports = {
+	saveBookRead,
+};
--- routes/booksRead.js
+++ routes/booksRead.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { getAllReadBooks, addBookRead } = require('../controllers/readController')
+const validation = require('../middleware/validate')
 
 router.get('/', getAllReadBooks)
 
-router.post('/', addBookRead)
+router.post('/', validation.saveBookRead, addBookRead);
 
 module.exports = router
